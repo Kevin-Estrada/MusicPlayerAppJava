50d91570de273fd21b754fdc9717f10d983090f0
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2021-12-22 00:15:23 -0600

    This is just adding some example classes to test out in the Main class

--- src/com/kevinestrada/test/MiniMiniMusicTest.java
+++ src/com/kevinestrada/test/MiniMiniMusicTest.java
@@ -8,38 +8,35 @@ public class MiniMiniMusicTest { // this is the first one
 
         try {
 
-            Sequencer player = MidiSystem.getSequencer();
-            player.open();
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
 
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
-            MidiEvent event = null;
+            for (int i = 5; i < 61; i += 4) {
+                track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(128, 1, i, 100, i + 2));
+            }
 
-            ShortMessage first = new ShortMessage();
-            first.setMessage(192, 1, instrument, 0);
-            MidiEvent changeInstrument = new MidiEvent(first, 1);
-            track.add(changeInstrument);
-
-            ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, note, 100);
-            MidiEvent noteOn = new MidiEvent(a, 1);
-            track.add(noteOn);
-
-            ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, note, 100);
-            MidiEvent noteOff = new MidiEvent(b, 16);
-            track.add(noteOff);
-            player.setSequence(seq);
-            player.start();
-            // new
-            Thread.sleep(5000);
-            player.close();
-            System.exit(0);
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     } // close play
+
+    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {
+        }
+        return event;
+    }
 
 } // close class
