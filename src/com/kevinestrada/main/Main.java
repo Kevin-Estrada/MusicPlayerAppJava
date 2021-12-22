8189d092e71618c4d4d278bb0ff3f09a82781d91
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2021-12-22 00:32:19 -0600

    This is to test out the next section of using Swing components to draw shapes based on the beat events

--- src/com/kevinestrada/main/Main.java
+++ src/com/kevinestrada/main/Main.java
@@ -4,14 +4,7 @@ import com.kevinestrada.test.MiniMiniMusicTest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("THis is the test");
         MiniMiniMusicTest mini = new MiniMiniMusicTest();
-        if (args.length < 2) {
-            System.out.println("Don't forget the instrument and note args");
-        } else {
-            int instrument = Integer.parseInt(args[0]);
-            int note = Integer.parseInt(args[1]);
-            mini.play(instrument, note);
-        }
+        mini.go();
     }
 }
